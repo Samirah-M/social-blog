f04dad09a6732b7df79398a6cafba76dcf2e6771
Samirah-M <143946932+Samirah-M@users.noreply.github.com>
2025-03-18 19:52:59 +0000

    qa page added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
+import QAPage from "./Components/QAPage";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/posts" element={<div>All Posts</div>} />
         <Route path="/login" element={<div>Login Page</div>} />
+        <Route path="/qa" element={<QAPage />} />
       </Routes>
     </Router>
   );

1b569acea60a9afc7dfab4da69a94718bf0c99f9
Samirah-M <143946932+Samirah-M@users.noreply.github.com>
2025-03-25 16:50:08 +0000

    imported correct style file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { StrictMode } from 'react';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import App from "./App";
 
-import './style.css';
+import "../src/index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
     <App />
